104e8544ff1b0db88524511da4e4dc75f575c7ad
Paul-Darius Sarmadi <st117748@ait.asia>
2015-12-06 19:30:52 +0700

    Options

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -33,6 +33,7 @@ class PurchasesController < ApplicationController
   # POST /purchases.json
   def create
     @purchase = Purchase.new(purchase_params)
+    @purchase.status = 0
     @purchase.user_id = current_user.id
     respond_to do |format|
       if @purchase.save
